5022481f56be9c4d40b6c41ff8686b6e3a02f11c
Jamil Burhan <Jamil.Burhan96@hotmail.com>
2015-02-19 18:57:53 -0800

    no message

---
+++ Excersises/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dating site - user enters sex, height, eye color  1 = blue 2=green 3=brown 4=black, hair color 1=white 2=blonde 3=red 4-brown 5=grey 6=black. Relationship status c v d or m. Do they like music yes or no. Do they like taking walks 
+            //outside ask for age. Their weight. How many kid they have. If they like sports yes or no. Do they like travenling. Yes or No.Say if the user can be associated to a profile. Profile number one - looking for men v or d maximum of one kid
+            //profile number 2 - looking for a girl c between 25 and 30 years old - blue eyes - dark hair. Likes music or sports or music and sports height between 5-6 feet. profile number 3 - looking for a women less than 25 years old if c less than 30 years old if v or d no kids that likes sports
+            //has red hair with green eyes or brown hair with brown eyes.
+
+            string gender;
+            double height;
+
+
+
+            Console.WriteLine("What is your gender?");
+            gender = Console.ReadLine();
+
+            Console.WriteLine("How tall are you?");
+            height = double.TryParse(Console.ReadLine());
+
+
+            
+            
+        }
+    }
+}
