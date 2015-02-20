fa241b6feebbedd1afcd6651d8bdc0180af7b8d2
Jamil Burhan <Jamil.Burhan96@hotmail.com>
2015-02-19 20:02:31 -0800

    no message

--- Excersises/Program.cs
+++ Excersises/Program.cs
@@ -15,19 +15,75 @@ namespace Excersises
             //profile number 2 - looking for a girl c between 25 and 30 years old - blue eyes - dark hair. Likes music or sports or music and sports height between 5-6 feet. profile number 3 - looking for a women less than 25 years old if c less than 30 years old if v or d no kids that likes sports
             //has red hair with green eyes or brown hair with brown eyes.
 
-            string gender;
-            double height;
-
+            string gender, eyecolor, haircolor, relationship, music, walks, traveling, sports;
+            double height, weight;
+            int kids, age;
+            bool verification; 
 
 
             Console.WriteLine("What is your gender?");
             gender = Console.ReadLine();
 
-            Console.WriteLine("How tall are you?");
-            height = double.TryParse(Console.ReadLine()); a
+            do
+            {
+                Console.WriteLine("How tall are you?");
+                verification = double.TryParse(Console.ReadLine(), out height);
+            }
+            while (verification == false);
+
+            Console.WriteLine("What is your eye color?");
+            Console.WriteLine("Blue=1\nGreen=2\nBrown=3\nBlack=4");
+            eyecolor = Console.ReadLine();
+
+            Console.WriteLine("What is your haircolor?");
+            haircolor = Console.ReadLine();
+
+            Console.WriteLine("What is your relationship status?\nC\nV\nd\nm");
+            relationship = Console.ReadLine();
+
+            Console.WriteLine("Do you like music?");
+            music = Console.ReadLine();
+
+            Console.WriteLine("Do you like long walks on the beach?");
+            walks = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("What is your weight?");
+                verification = double.TryParse(Console.ReadLine(), out weight);
+            }
+            while (verification == false);
+
+            do
+            {
+                Console.WriteLine("What is your age?");
+                verification = int.TryParse(Console.ReadLine(), out age);
+            }
+            while (verification == false);
+
+            do
+            {
+                Console.WriteLine("How many kids do you have?");
+                verification = int.TryParse(Console.ReadLine(), out kids);
+
+            }
+            while (verification == false);
+
+            Console.WriteLine("Do you like traveling?");
+            traveling = Console.ReadLine();
+
+            Console.WriteLine("Do you like sports?");
+            sports = Console.ReadLine();
 
 
-            
+            //Comparing 
+
+            if ((relationship == "v" || relationship == "V" || relationship == "d" || relationship == "D") && kids == 1)
+                Console.WriteLine("You are matched with profile 1!");
+
+            if ((gender == "female" || gender == "Female" || gender == "girl" || gender == "Girl") && (relationship == "c" || relationship == "C") && (age >= 25 || age <= 30) && (eyecolor == "1") && (haircolor == "dark" || haircolor == "black" || haircolor == "Black" ) )
+                Console.WriteLine("You are matched with profile 2!");
+            Console.ReadLine();
             
         }
     }
